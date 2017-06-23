749aaed1013591a273a9c538bdc7278eb2b57ce2
Carson Killbreath <kill4992@kettering.edu>
2017-06-23 12:24:01 -0500

    Fix indexing on order reducers

--- app/reducers/order.js
+++ app/reducers/order.js
@@ -1,59 +1,48 @@
-import {
-  ADD_ITEM_TO_ORDER,
-  REMOVE_ITEM_FROM_ORDER,
-  SUBMIT_ORDER,
-  SUBMIT_ORDER_SUCCESS,
-  SUBMIT_ORDER_FAIL,
-  CLEAR_ORDER_SUCCESS
-} from "../actions";
+import { ADD_ITEM_TO_ORDER, REMOVE_ITEM_FROM_ORDER, SUBMIT_ORDER, SUBMIT_ORDER_SUCCESS, SUBMIT_ORDER_FAIL, CLEAR_ORDER_SUCCESS } from '../actions'
 
 const defaultOrderState = {
   items: [],
   submittingOrder: false,
   orderSuccess: false,
-  currentIndex: 1
-};
+  currentIndex: 0
+}
 
 export default function order(state = defaultOrderState, action) {
   switch (action.type) {
     case ADD_ITEM_TO_ORDER:
-      const currentIndex = state.currentIndex + 1;
-
       const addedItem = {
-        index: currentIndex,
+        index: state.currentIndex,
         itemId: action.itemId,
         addIngredients: action.addIngredients,
         removeIngredients: action.removeIngredients
-      };
+      }
 
       return {
         ...state,
-        currentIndex,
+        currentIndex: state.currentIndex + 1,
         items: [...state.items, addedItem]
-      };
+      }
     case REMOVE_ITEM_FROM_ORDER:
       return {
         ...state,
-        items: state.items.filter(item => {
-          item.index !== action.itemIndex;
-        })
-      };
+        items: state.items.filter((item) => { item.index !== action.itemIndex })
+      }
     case SUBMIT_ORDER:
       return {
         ...state,
         submittingOrder: true
-      };
+      }
     case SUBMIT_ORDER_SUCCESS:
       return {
         ...state,
         submittingOrder: false,
         orderSuccess: true
-      };
+      }
     case CLEAR_ORDER_SUCCESS:
       return {
         ...state,
         orderSuccess: false
-      };
+      }
     default:
       return state;
   }
